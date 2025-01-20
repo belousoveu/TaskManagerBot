44bbfd71a566a2e710b3868affa179f3c18c7db4
Belousoveu <belousov.eu@gmail.com>
2025-01-20 11:44:22 +0300

    Initial commit TelegramBotCore Module

---
+++ telegramBotCore/src/main/java/com/github/belousovea/telegrambotcore/TelegramBotCoreApplication.java
@@ -0,0 +1,13 @@
+package com.github.belousovea.telegrambotcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotCoreApplication.class, args);
+	}
+
+}
---
+++ telegramBotCore/src/test/java/com/github/belousovea/telegrambotcore/TelegramBotCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.belousovea.telegrambotcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
