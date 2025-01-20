e59bf6f60a78cc20e5bda8417f3c002f8d98af47
Belousoveu <belousov.eu@gmail.com>
2025-01-20 15:41:54 +0300

    initial commit module BotWithPostgres

---
+++ botWithPostgres/src/main/java/com/github/belousovea/botwithpostgres/BotWithPostgresApplication.java
@@ -0,0 +1,13 @@
+package com.github.belousovea.botwithpostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotWithPostgresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BotWithPostgresApplication.class, args);
+    }
+
+}
---
+++ botWithPostgres/src/test/java/com/github/belousovea/botwithpostgres/BotWithPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.belousovea.botwithpostgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotWithPostgresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
